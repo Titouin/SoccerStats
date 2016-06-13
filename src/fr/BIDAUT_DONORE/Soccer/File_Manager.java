b84de333f29d37d7c2a1522b30d8454ab2059f68
Titouin <nicolas.donore@gmail.com>
2016-06-13 14:01:16 +0200

    Init

---
+++ src/fr/BIDAUT_DONORE/Soccer/File_Manager.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fr.BIDAUT_DONORE.Soccer;
+
+/**
+ * @author donore
+ *
+ */
+public class File_Manager {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
